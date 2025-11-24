2e88085eed097ef2a1e1345951fa37665caf0756
AntonC9018 <anton.curm@gmail.com>
2025-11-24 15:51:44 +0200

    Video 1

--- examples/dependencies_simplified/code/Code.cs
+++ examples/dependencies_simplified/code/Code.cs
@@ -1,3 +1,38 @@
 public static class Helper
 {
+    public static List<Item> ProcessItems(
+        List<Item> items,
+        float minPriceCutoff,
+        float maxPriceCutoff,
+        Dictionary<string, string> nameRemap)
+    {
+        var ret = new List<Item>();
+        foreach (var item in items)
+        {
+            if (item.Price <= minPriceCutoff)
+            {
+                continue;
+            }
+            if (item.Price >= maxPriceCutoff)
+            {
+                continue;
+            }
+            string name = item.Name;
+            name = nameRemap.GetValueOrDefault(name, name);
+            name = name.ToUpper();
+
+            ret.Add(new Item
+            {
+                Name = name,
+                Price = item.Price,
+            });
+        }
+        return ret;
+    }
+}
+
+public sealed class Item
+{
+    public required string Name;
+    public required float Price;
 }
