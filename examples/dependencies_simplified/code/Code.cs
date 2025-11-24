407acfc8643e7a38c4031696e2eaa858541a46e4
AntonC9018 <anton.curm@gmail.com>
2025-11-24 16:08:59 +0200

    Added config dependency

--- examples/dependencies_simplified/code/Code.cs
+++ examples/dependencies_simplified/code/Code.cs
@@ -1,31 +1,60 @@
 public static class Helper
 {
+    private static Item? ProcessItem(
+        Item item,
+        float minPriceCutoff,
+        float maxPriceCutoff,
+        Dictionary<string, string> nameRemap,
+        HashSet<string> ignoredNames)
+    {
+        if (item.Price <= minPriceCutoff)
+        {
+            return null;
+        }
+        if (item.Price >= maxPriceCutoff)
+        {
+            return null;
+        }
+        string name = item.Name;
+        if (ignoredNames.Contains(name))
+        {
+            return null;
+        }
+
+        name = nameRemap.GetValueOrDefault(name, name);
+        name = name.ToUpper();
+
+        var newItem = new Item
+        {
+            Name = name,
+            Price = item.Price,
+        };
+        return newItem;
+    }
+
+    // Single Responsibility
     public static List<Item> ProcessItems(
         List<Item> items,
         float minPriceCutoff,
         float maxPriceCutoff,
-        Dictionary<string, string> nameRemap)
+        Dictionary<string, string> nameRemap,
+        HashSet<string> ignoredNames)
     {
         var ret = new List<Item>();
         foreach (var item in items)
         {
-            if (item.Price <= minPriceCutoff)
+            var newItem = ProcessItem(
+                // item -> прямая зависимость - параметр
+                item,
+                // конфигурация
+                minPriceCutoff,
+                maxPriceCutoff,
+                nameRemap,
+                ignoredNames);
+            if (newItem != null)
             {
-                continue;
+                ret.Add(newItem);
             }
-            if (item.Price >= maxPriceCutoff)
-            {
-                continue;
-            }
-            string name = item.Name;
-            name = nameRemap.GetValueOrDefault(name, name);
-            name = name.ToUpper();
-
-            ret.Add(new Item
-            {
-                Name = name,
-                Price = item.Price,
-            });
         }
         return ret;
     }
