3181c0c4dff4f4931df21a7fb283b059aacecb5a
AntonC9018 <anton.curm@gmail.com>
2025-11-24 15:51:44 +0200

    Video 1

--- examples/dependencies_simplified/tests/Tests.cs
+++ examples/dependencies_simplified/tests/Tests.cs
@@ -1,4 +1,45 @@
 ﻿public sealed class Tests
 {
+    [Fact]
+    public async Task Test1()
+    {
+        var items = new List<Item>
+        {
+            new()
+            {
+                Name = "Anton",
+                Price = 40.0f,
+            },
+            new()
+            {
+                Name = "Game",
+                Price = 1.0f,
+            },
+            new()
+            {
+                Name = "Fork",
+                Price = 26.0f,
+            },
+            new()
+            {
+                Name = "Yellow",
+                Price = 90.0f,
+            },
+            new()
+            {
+                Name = "lower-case",
+                Price = 40.0f,
+            },
+        };
 
+        var result = Helper.ProcessItems(items,
+            minPriceCutoff: 5.0f,
+            maxPriceCutoff: 50.0f,
+            nameRemap: new()
+            {
+                ["Anton"] = "Mark",
+            });
+
+        await Verify(result);
+    }
 }
